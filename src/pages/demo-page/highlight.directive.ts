8efd241adc14dc8681900b66fee64e9412dc094a
JasperMao123 <jaspermao@icloud.com>
2017-05-15 17:31:09 +0800

    background

--- src/pages/demo-page/highlight.directive.ts
+++ src/pages/demo-page/highlight.directive.ts
@@ -1,13 +1,14 @@
 /**
  * Created by Jasper on 2017/5/11.
  */
-import {Directive, ElementRef,Renderer} from '@angular/core';
+import {Directive, ElementRef} from '@angular/core';
 
 @Directive({
   selector:'[myhighlight]',
 })
 export class HighLightDirective{
-  constructor(el: ElementRef,render: Renderer){
-    render.setElementStyle(el.nativeElement,'backgroundColor','yellow');
+  constructor(el: ElementRef){
+    //render.setElementStyle(el.nativeElement,'backgroundColor','yellow');
+    el.nativeElement.style.backgroundColor = 'yellow';
   }
 }
